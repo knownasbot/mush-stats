b471113b351bedad2b6ad94ca579b0ed925d61d8
BOT <42821200+knownasbot@users.noreply.github.com>
2023-12-27 13:05:25 -0300

    Adicionado dois novos modos

--- src/functions/game-fields/buildbattle.ts
+++ src/functions/game-fields/buildbattle.ts
@@ -79,6 +79,110 @@ export default function getBuildBattleFields(
                     },
                 ],
             },
+            {
+                title: "Solo",
+                fields: [
+                    {
+                        name: "Jogos",
+                        value: stats?.buildbattle?.solo_played ?? 0,
+                    },
+                    {
+                        name: "Vitórias",
+                        value: stats?.buildbattle?.solo_wins ?? 0,
+                    },
+                    {
+                        name: "Derrotas",
+                        value: stats?.buildbattle?.solo_losses ?? 0,
+                    },
+                    {
+                        name: "Construções Perfeitas",
+                        value: stats?.buildbattle?.solo_perfects ?? 0,
+                    },
+                    {
+                        name: "Primeiro Lugar",
+                        value: stats?.buildbattle?.solo_first_place ?? 0,
+                    },
+                    {
+                        name: "Segundo Lugar",
+                        value: stats?.buildbattle?.solo_second_place ?? 0,
+                    },
+                    {
+                        name: "Terceiro Lugar",
+                        value: stats?.buildbattle?.solo_third_place ?? 0,
+                    },
+                    {
+                        name: "Pontos",
+                        value: stats?.buildbattle?.solo_points ?? 0,
+                    },
+                    {
+                        name: "Winstreak",
+                        value: stats?.buildbattle?.solo_winstreak ?? 0,
+                    },
+                    {
+                        name: "Winstreak Recorde",
+                        value: stats?.buildbattle?.solo_max_winstreak ?? 0,
+                    },
+                    {
+                        name: "W/L",
+                        value: getRate(
+                            stats?.buildbattle?.solo_wins,
+                            stats?.buildbattle?.solo_losses
+                        ),
+                    },
+                ],
+            },
+            {
+                title: "Duplas",
+                fields: [
+                    {
+                        name: "Jogos",
+                        value: stats?.buildbattle?.doubles_played ?? 0,
+                    },
+                    {
+                        name: "Vitórias",
+                        value: stats?.buildbattle?.doubles_wins ?? 0,
+                    },
+                    {
+                        name: "Derrotas",
+                        value: stats?.buildbattle?.doubles_losses ?? 0,
+                    },
+                    {
+                        name: "Construções Perfeitas",
+                        value: stats?.buildbattle?.doubles_perfects ?? 0,
+                    },
+                    {
+                        name: "Primeiro Lugar",
+                        value: stats?.buildbattle?.doubles_first_place ?? 0,
+                    },
+                    {
+                        name: "Segundo Lugar",
+                        value: stats?.buildbattle?.doubles_second_place ?? 0,
+                    },
+                    {
+                        name: "Terceiro Lugar",
+                        value: stats?.buildbattle?.doubles_third_place ?? 0,
+                    },
+                    {
+                        name: "Pontos",
+                        value: stats?.buildbattle?.doubles_points ?? 0,
+                    },
+                    {
+                        name: "Winstreak",
+                        value: stats?.buildbattle?.doubles_winstreak ?? 0,
+                    },
+                    {
+                        name: "Winstreak Recorde",
+                        value: stats?.buildbattle?.doubles_max_winstreak ?? 0,
+                    },
+                    {
+                        name: "W/L",
+                        value: getRate(
+                            stats?.buildbattle?.doubles_wins,
+                            stats?.buildbattle?.doubles_losses
+                        ),
+                    },
+                ],
+            },
         ],
     };
 }
