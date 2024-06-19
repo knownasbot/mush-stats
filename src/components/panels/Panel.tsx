7e625199049273f263b791f1ee8543bafaaed4d8
BOT <42821200+knownasbot@users.noreply.github.com>
2024-06-19 18:22:23 -0300

    Ajustado sombra de paineis

--- src/components/panels/Panel.tsx
+++ src/components/panels/Panel.tsx
@@ -5,7 +5,7 @@ const Panel = styled.div`
     background: #fcfcfc;
     border: 1px solid rgba(0, 0, 0, 0.08);
     border-radius: 25px;
-    box-shadow: 0 3px 10px rgba(0, 0, 0, 0.12);
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.05);
     box-sizing: border-box;
 `;
 
